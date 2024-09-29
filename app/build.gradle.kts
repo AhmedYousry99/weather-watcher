6abbd56767ab5cf3761a7a97df5340aeafdb5f23
Khaatora <ahosari20@gmail.com>
2024-09-29 08:42:06 +0300

    build files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id ("kotlin-kapt")
 }
 
 android {
@@ -9,14 +12,24 @@ android {
 
     defaultConfig {
         applicationId = "com.senseicoder.weatherwatcher"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val properties=Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        buildConfigField ("String","API_KEY","\"${properties.getProperty("API_KEY")}\"")
+        buildConfigField ("String","UV_API_KEY","\"${properties.getProperty("UV_API_KEY")}\"")
+        buildConfigField ("String","CITIES_API_KEY","\"${properties.getProperty("CITIES_API_KEY")}\"")
     }
 
+    buildFeatures{
+        viewBinding = true
+        buildConfig = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -42,7 +55,63 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+//RETROFIT
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+//GSON
+    implementation(libs.gson)
+//ROOM
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.room.compiler)
+//GLIDE
+    implementation(libs.glide)
+    kapt(libs.glide.compiler)
+//ViewModel
+    implementation (libs.androidx.lifecycle.extensions)
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+//open street map
+    implementation (libs.osmdroid.android)
+//Work Manager
+    implementation (libs.androidx.work.runtime.ktx)
+//Testing
+    // Unit Testing
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.hamcrest:hamcrest-all:1.3")
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
+    testImplementation("org.robolectric:robolectric:4.8")
+
+    // AndroidX Test - JVM Testing
+    testImplementation("androidx.test:core-ktx:1.4.0")
+    testImplementation("androidx.test.ext:junit-ktx:1.1.3")
+
+/*    // AndroidX Test - Instrumented Testing
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")*/
+
+    // InstantTaskExecutorRule
+    testImplementation("androidx.arch.core:core-testing:2.1.0")
+    androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
+
+    // Coroutines Testing
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
+
+    // Mokito
+    testImplementation("org.mockito:mockito-core:3.10.0") 
+    androidTestImplementation("org.mockito:mockito-android:3.10.0")
+
+    // Hamcrest for assertions
+    testImplementation("org.hamcrest:hamcrest:2.2")
+    testImplementation("org.hamcrest:hamcrest-library:2.2")
+    androidTestImplementation("org.hamcrest:hamcrest:2.2")
+    androidTestImplementation("org.hamcrest:hamcrest-library:2.2")
 }
